2e34bf4a6edc837f9d04a77003118dc8667978d0
Mathew C <135055113+MathCRojas@users.noreply.github.com>
2023-07-08 09:34:08 -0600

    Update app.js

--- cache-with-network/js/app.js
+++ cache-with-network/js/app.js
@@ -1,5 +1,5 @@
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('https://matcrojas.github.io/Aplicaciones-Web-Progresivas/10b/practica5/sw.js');
+    navigator.serviceWorker.register('https://mathcrojas.github.io/Aplicaciones-Web-Progresivas/cache-with-network/sw.js');
 } else {
     console.log('No se permite SW');
 }
